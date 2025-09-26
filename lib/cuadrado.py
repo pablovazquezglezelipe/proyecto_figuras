0a60d768849a4222a4165c6fa16dec7f7c663afa
Pablo Vazquez Gonzalez-Elipe <pablovazquezgelipe@gmail.com>
2025-09-26 12:08:13 +0200

    Creadas funciones del triangulo

--- lib/cuadrado.py
+++ lib/cuadrado.py
@@ -1,6 +1,6 @@
 def get_area(lado: int) -> int:
 	return lado*lado
-def get_identificador() -> str):
+def get_identificador() -> str:
 	return "cuadrado"
-def get_perimetro(lado: int) -> int):
+def get_perimetro(lado: int) -> int:
 	return 4*lado
